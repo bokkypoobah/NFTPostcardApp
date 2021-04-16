8c50bee1f22b8cb8d90f6a1a361bb4b5e93499d9
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2021-04-16 14:48:11 +1000

    WIP

--- docs/docs.js
+++ docs/docs.js
@@ -1,7 +1,7 @@
 const Docs = {
   template: `
     <div class="mt-5 pt-3">
-      <b-card no-body header="Documentation" class="border-0" header-class="p-1">
+      <b-card no-body header="Info" class="border-0" header-class="p-1">
         <b-card no-body class="border-0 m-0 mt-2">
           <b-tabs v-model="section" pills card vertical end nav-class="m-1 p-1" active-tab-class="m-1 mt-2 p-1">
 
@@ -9,6 +9,115 @@ const Docs = {
             <b-tab title="Intro" @click.prevent="updateRouterParamsSectionTopic('intro', 'top')">
               <b-card-text>
                 <h5 ref="intro_top" class="mb-3">Intro</h5>
+
+                <p>Zombies <b-link href="https://www.larvalabs.com/cryptopunks/details/3636" class="card-link" target="_blank">#3636 <b-avatar variant="light" size="2.0rem" src="https://www.larvalabs.com/public/images/cryptopunks/punk3636.png"></b-avatar></b-link> and
+                <b-link href="https://www.larvalabs.com/cryptopunks/details/4472" class="card-link ml-0" target="_blank">#4472 <b-avatar variant="light" size="2.0rem" src="https://www.larvalabs.com/public/images/cryptopunks/punk4472.png"></b-avatar></b-link> Xtreme High Yield cultivators</p>
+
+                <b-card no-body header="Zombies #3636 & #4472" class="border-0" header-class="p-1">
+                  <div class="m-5">
+                      <b-carousel
+                        id="carousel-1"
+                        v-model="slide"
+                        :interval="5000"
+                        controls
+                        indicators
+                        background="#ababab"
+                        img-width="1024"
+                        img-height="480"
+                        style="text-shadow: 2px 2px 3px #333;"
+                        @sliding-start="onSlideStart"
+                        @sliding-end="onSlideEnd"
+                        class="mx-5 p-0"
+                      >
+
+                        <!-- Text slides with image -->
+                        <b-carousel-slide caption="Zombie #3636,"
+                          text="BASTARD GAN V2 Children And Cats, Bombo, NSW, Australia, Year 2021">
+                          <b-img-lazy slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+                             src="images/IMG_9534_z3636_Bombo_2048x960.png" alt="image slot"></b-img-lazy>
+                        </b-carousel-slide>
+
+                        <b-carousel-slide caption="Zombie #3636 & #4472 Family"
+                          text="On A Palaeontological Stroll Down The Permian At Gerroa, NSW, Australia">
+                          <b-img-lazy slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+                               src="images/GerroaPhotoAlbumWithZ3636n4472Family_2048x960.png" alt="image slot"></b-img-lazy>
+                        </b-carousel-slide>
+
+                        <b-carousel-slide caption="Zombie #3636 & #4472 Family"
+                          text="In Year 1637 At Utrecht To Trade Tulip NFT Options">
+                          <b-img-lazy slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+                               src="images/Cryptogs_3185_ZFam_2048x960.png" alt="image slot"></b-img-lazy>
+                        </b-carousel-slide>
+
+                        <b-carousel-slide caption="Zombie #3636 & #4472 Family"
+                          text="In 1507 At Badaling To Trade Silk NFTs">
+                          <b-img-lazy slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+                               src="images/GreatWall_ZFam_2048x960.png" alt="image slot"></b-img-lazy>
+                        </b-carousel-slide>
+
+                        <b-carousel-slide caption="Zombie Xtreme High Yield Farmers"
+                          text="With Subjects In 1935 At Milsons Point">
+                          <b-img-lazy slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+                               src="images/PunkstersHarbourBridgeMilsonsPoint1935_1600x750.png" alt="image slot"></b-img-lazy>
+                        </b-carousel-slide>
+
+                        <b-carousel-slide caption="Zombie #3636 & #4472"
+                          text="Infected By Airborne Z-Alien Virus, Travel To 1,050 BC To Inspect Their Re-analoged Digitalised Cat At Earth-619">
+                          <b-img-lazy slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+                               src="images/Punks_3636_4472_Sphinx_1024x480.png" alt="image slot"></b-img-lazy>
+                        </b-carousel-slide>
+
+                        <b-carousel-slide caption="Zombie Xtreme High Yield Cultivators And Subjects"
+                          text="Infected By Mutated Z-Alien Virus Strains In 1,050 BC at Earth-619">
+                          <b-img-lazy slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+                               src="images/Punks_3636_4472_Sphinx_Subjects_More_1024x480.png" alt="image slot"></b-img-lazy>
+                        </b-carousel-slide>
+
+                        <b-carousel-slide caption="Zombie Xtreme High Yield Cultivators"
+                          text="Take Ownership Of Twins At 1888 In The Royal Prince Alfred Hospital, Sydney. Credits - Mitchell Library, State Library of NSW">
+                          <b-img-lazy slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+                               src="images/CryptoBabyPunk_401_Birth_At_RPA_1880-1893_960x450.png" alt="image slot"></b-img-lazy>
+                        </b-carousel-slide>
+
+
+
+                        <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+                        <!--
+                        <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+                            a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+                          </p>
+                        </b-carousel-slide>
+                        -->
+
+                      </b-carousel>
+
+                      <p class="mt-4">
+                        Slide #: {{ slide }}<br>
+                        Sliding: {{ sliding }}
+                      </p>
+                    </div>
+
+
+                    <!--
+                    <b-card-body class="mb-1">
+                      <b-list-group>
+                        <b-list-group-item to="/optinoExplorer/all">Optino Explorer</b-list-group-item>
+                        <b-list-group-item to="/feedsExplorer/all">Feeds Explorer</b-list-group-item>
+                        <b-list-group-item to="/tokensExplorer/all">Tokens Explorer</b-list-group-item>
+                        <b-list-group-item to="/governance/all">Governance</b-list-group-item>
+                        <b-list-group-item href="https://wiki.optino.io" target="_blank">Wiki</b-list-group-item>
+                      </b-list-group>
+                      <b-card-text class="mt-5">
+                        This is still work in progress. You will need a browser with web3 injection, e.g., using the MetaMask addon. In your web3 wallet, switch to the Ropsten testnet.
+                        <br />
+                        <div v-if="!connect">Please click on the power button <b-icon-power variant="primary" shift-v="-1" font-scale="1.5"></b-icon-power> on the top right to connect via MetaMask.</div>
+                      </b-card-text>
+                    </b-card-body>
+                    -->
+                  </b-card>
+
 
                 <p>The Optino Protocol is a set of Ethereum smart contracts that allows option sellers to escrow collateral into blah</p>
 
@@ -371,6 +480,8 @@ contract OptinoFormulae is DataType {
   data: function () {
     return {
       section: 2,
+      slide: 0,
+      sliding: null,
     }
   },
   computed: {
@@ -409,6 +520,12 @@ contract OptinoFormulae is DataType {
     }
   },
   methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
+    },
     updateRouterParamsSectionTopic(section, topic) {
       logInfo("Docs", "updateRouterParamsSectionTopic(" + JSON.stringify(section) + ", " + topic + ")");
       this.$router.push({ params: { section: section, topic: topic }}).catch(err => {});
